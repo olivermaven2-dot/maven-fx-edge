b6b3f3badcbe2f44fbae3504a729ecfd99dd7dcc
olivermaven2-dot <olivermaven2@gmail.com>
2026-09-21 16:39:08 +0300

    Refresh Maven FX PWA cache after WebSocket fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="maven-fx-v1";
+const CACHE="maven-fx-v2";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
