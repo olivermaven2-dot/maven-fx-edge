4e0d1ed53f164822f2b094008c09937560df8fac
olivermaven2-dot <olivermaven2@gmail.com>
2026-09-22 09:52:04 +0300

    Refresh Maven FX connection build

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="maven-fx-v5";
+const CACHE="maven-fx-v6";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
